039c041f3cce1bc7b9d9c42f3b64074ad8e09c94
cjfcsjt <jingfanchen77@gmail.com>
2026-04-04 13:56:16 +0800

    fix ray bug

--- train/ray_pretrain_unified_navit_vae.py
+++ train/ray_pretrain_unified_navit_vae.py
@@ -960,6 +960,7 @@ def main():
             "WANDB_ENTITY": os.environ.get("WANDB_ENTITY", ""),
             "WANDB_PROJECT": os.environ.get("WANDB_PROJECT", ""),
             "HF_HOME": os.environ.get("HF_HOME", ""),
+            "PATH": "/mnt/group/jingfanchen/miniconda3/envs/ray_py311/bin:" + os.environ.get("PATH", "")
         },
     }
     # 初始化 Ray 集群连接
